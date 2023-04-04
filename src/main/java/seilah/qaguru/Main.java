48c5c60ee4abb729e37c63b873cfa2c528dec0fa
seilah <princenoru@gmail.com>
2023-04-04 13:16:13 +0400

    second commit

--- src/main/java/seilah/qaguru/Main.java
+++ src/main/java/seilah/qaguru/Main.java
@@ -2,6 +2,6 @@ package seilah.qaguru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("some text");
     }
 }
